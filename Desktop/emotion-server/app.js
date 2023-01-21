12788745a5f9af42cc64e9362db1181ee9b68eef
kang seok <sekeipost@naver.com>
2023-01-21 15:34:14 +0900

    feat : add emotion endpoint that receives the text and return the emotion

--- Desktop/emotion-server/app.js
+++ Desktop/emotion-server/app.js
@@ -2,14 +2,14 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const express = require("express");
 const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
-const token =
-  "ya29.a0AX9GBdXxSt3G3-xGrBATiXJCOqxIlJDSfe6ygq0Lw3O6NelI2qlA3Ne2zs0l00cLDfoTT_KxPDHGDtHrWr_vJCfXQXpFoCgauSHXKgzUr-DyIZGrYoFuOHrdWez9opo39w7cK1sNUiCnNS76i_EFqUOKZZxf-OJoaCgYKASgSAQASFQHUCsbC6B5ZoeckzmNkVY9XjwZsrQ0167";
-
-let newToken = "";
+app.use(cors());
+app.use(bodyParser.json());
 
 async function getToken() {
   try {
@@ -26,8 +26,9 @@ async function getToken() {
   }
 }
 
-async function submit(text) {
+async function getSentiment(text) {
   const newToken = await getToken();
+  console.log(newToken);
   const body = {
     encodingType: "UTF8",
     document: {
@@ -35,23 +36,118 @@ async function submit(text) {
       content: text,
     },
   };
-  const response = await axios.post(`${newToken}`, body, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json; charset=utf-8",
-      "x-goog-user-project": "projectforaeye",
-    },
-  });
-
+  const response = await axios.post(
+    "https://language.googleapis.com/v1/documents:analyzeSentiment",
+    body,
+    {
+      headers: {
+        Authorization: `Bearer ${newToken.trim()}`,
+        "Content-Type": "application/json; charset=utf-8",
+        "x-goog-user-project": "projectforaeye",
+      },
+    }
+  );
+  console.log(response.data);
   return response.data;
 }
 
-app.post("/emotion", async (req, res) => {
-  const { text } = req.body;
-  const response = await submit(text);
-  const { documentSentiment } = response.data;
+function getSentimentAvg(sentences) {
+  const totalStatic = sentences.reduce(
+    (acc, cur) => {
+      const { magnitude, score } = cur.sentiment;
+      acc.totalScore += magnitude * score;
+      if (score > 0) {
+        const { positive } = acc;
+        positive.totalMagnitude += magnitude;
+        positive.totalScore += score;
+        positive.number += 1;
+      } else if (score < 0) {
+        const { negative } = acc;
+        negative.totalMagnitude += magnitude;
+        negative.totalScore += score;
+        negative.number += 1;
+      }
+      return acc;
+    },
+    {
+      totalScore: 0,
+      positive: {
+        totalMagnitude: 0,
+        totalScore: 0,
+        number: 0,
+      },
+      negative: {
+        totalMagnitude: 0,
+        totalScore: 0,
+        number: 0,
+      },
+    }
+  );
+  const getAvg = (curEmotion) => {
+    const [magnitudeAvg, scoreAvg] = [
+      curEmotion.totalMagnitude / curEmotion.number,
+      curEmotion.totalScore / curEmotion.number,
+    ];
+    return { magnitudeAvg: magnitudeAvg, scoreAvg: scoreAvg };
+  };
+  const { totalScore } = totalStatic;
+  const curEmotion =
+    totalScore >= 0 ? totalStatic.positive : totalStatic.negative;
+  const result = getAvg(curEmotion);
 
-  res.send(response);
+  return result;
+}
+
+const getEmotion = (magnitude, score) => {
+  if (score > 0.5) {
+    if (magnitude >= 0.75) {
+      return "excited";
+    } else if (magnitude > 0.5) {
+      return "happy";
+    } else if (magnitude > 0.25) {
+      return "content";
+    } else {
+      return "relaxed";
+    }
+  } else if (score > 0.1) {
+    if (magnitude > 0.25) {
+      return "surprised(good reason)";
+    } else {
+      return "calm";
+    }
+  } else if (score >= -0.1) {
+    return "anticiapte";
+  } else if (score >= -0.5) {
+    if (magnitude > 0.25) {
+      return "surprised(bad reason)";
+    } else {
+      return "tired";
+    }
+  } else {
+    if (magnitude >= 0.75) {
+      return "tense";
+    } else if (magnitude > 0.5) {
+      return "angry";
+    } else if (magnitude > 0.25) {
+      return "sad";
+    } else {
+      return "bored";
+    }
+  }
+};
+
+app.post("/emotion", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { text } = req.body;
+    const { sentences } = await getSentiment(text);
+    const { magnitudeAvg, scoreAvg } = getSentimentAvg(sentences);
+    console.log(magnitudeAvg, scoreAvg);
+    const response = getEmotion(magnitudeAvg, scoreAvg);
+    res.send({ sentences: sentences, response: response });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(port, () => {
